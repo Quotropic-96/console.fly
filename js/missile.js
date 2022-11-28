c56ebe7faf363f6415f1e1621a2806590b7f3364
Gerard Solanes Hernandez <gerardsolher@gmail.com>
2022-11-28 12:46:38 +0100

    Complete Missile.moveLeft()

--- js/missile.js
+++ js/missile.js
@@ -4,7 +4,7 @@ class Missile {
         this.y = y;
         this.width = width;
         this.height = height;
-        this.speed;
+        this.speed = 20;
         this.isAlerting = true;
         this.color = rgb(255,0,0);
     }
@@ -14,5 +14,11 @@ class Missile {
             this.isAlerting = false;
             this.color = rgb(0,0,0);
         },1000);
+    }
+
+    _moveLeft() {
+        if (!this.isAlerting && this.x + this.width > 0) {
+            this.x -= this.speed;
+        }
     }
 }
