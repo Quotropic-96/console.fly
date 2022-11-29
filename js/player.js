5481e6da2cbc95aed2370dc26517248c7ce88d7b
Gerard Solanes Hernandez <gerardsolher@gmail.com>
2022-11-29 20:37:42 +0100

    Gravitationall pull implemented. May need to refactor fly for uppwards acceleration

--- js/player.js
+++ js/player.js
@@ -5,15 +5,32 @@ class Player {
         this.width = width;
         this.height = height;
         this.isFly = false;
+        this.isFalling = false;
+        this.fallCount = 1;
     }
     fly() {
+        this.isFalling = false;
+        this.fallCount = 1;
         if (this.y > 0) {
             this.y -= 10;
         }
     }
     fall() {
-        if (this.y + this.height < 600) {
-            this.y += 10;
+        let yIncrement = 0.5*3*(2*this.fallCount-1);
+        if (this.y + this.height + yIncrement < 600) {
+            this.isFalling = true;
+            this.y += yIncrement;
+        } else {
+            this.y = 600 - this.height;
+            this.isFalling = false;
+        }
+        this._fallCount();
+    }
+    _fallCount() {
+        if (this.isFalling) {
+            this.fallCount += 1;
+        } else {
+            this.fallCount = 1;
         }
     }
 }
