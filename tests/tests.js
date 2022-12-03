4c184f9ded65266d9126e04d1a94d2e11538625e
Gerard Solanes Hernandez <gerardsolher@gmail.com>
2022-12-03 11:18:50 +0100

    Add new assets and complete tests for parallax. ready to implement to main game

--- tests/tests.js
+++ tests/tests.js
@@ -1,20 +1,34 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+const bgImg0 = new Image();
+bgImg0.src = '../assets/bg/bg_0.png';
+
 const bgImg1 = new Image();
-bgImg1.src = '../assets/Bg/1.png';
+bgImg1.src = '../assets/bg/bg_1.png';
+
+const bgImg2 = new Image();
+bgImg2.src = '../assets/bg/bg_2.png';
+
+const bgImg3 = new Image();
+bgImg3.src = '../assets/bg/bg_3.png';
+
 
 function _parallaxAnimation(img) {
-    let dx = 0;
-    setInterval(() => {
-        if (dx === 1000) {
-            dx = 0;
-        }
-        console.log(dx);
-        ctx.drawImage(img, 0 - dx, 0, 1000, 600);
-        ctx.drawImage(img, 1000 - dx, 0, 1000, 600);
-        dx++;
-    },10);
+    if (dx === 1000) {
+        dx = 0;
+    }
+    console.log(dx);
+    ctx.drawImage(img, 0 - dx, 0, 1000, 600);
+    ctx.drawImage(img, 1000 - dx, 0, 1000, 600);
+    dx++;
 };
 
-_parallaxAnimation(bgImg1);
+let dx = 0;
+setInterval(() => {
+    _parallaxAnimation(bgImg0);
+    _parallaxAnimation(bgImg1);
+    _parallaxAnimation(bgImg2);
+    _parallaxAnimation(bgImg3);
+},40)
+
