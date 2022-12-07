98ab84e9e1d8506a258f46643b49a8d6978480f1
Gerard Solanes Hernandez <gerardsolher@gmail.com>
2022-12-07 18:01:25 +0100

    Implement instructions page logic in scripts.js. Ready for css refactor

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+  // Main set up
   const gamePage = document.getElementById('game-page');
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
@@ -8,14 +9,62 @@ window.onload = function () {
   const meters = document.getElementById('meter-number');
   const losePage = document.getElementById('lose-page');
   const resultMeters = document.getElementById('result-number');
+
+  // Instructions page set up
+  const instructionsAudio = createSound(instructionsMusic);
+  const instructionsAnimation = playerIdle;
+  const txt = ['In the middle of the misty Red Mountains lived a young developer.','All his teachers had warned him to be mindful of the power of the console.','Then came the update...','Press SPACE BAR for console.fly()','He pressed it and unleashed the true power of the console.','How far will he get?'];
+  const txtDiv = document.getElementById('instructions-text');
+  const instructionsCanvas = document.getElementById('instructions-canvas');
+  const instructionsCtx = instructionsCanvas.getContext('2d');
+  let letterIdx = 0;
+  let lineIdx = 0;
+  let animationCount = 0;
+
+  // Instructions page functions
+  function typingEffect() {
+    let p = document.createElement('p');
+    p.setAttribute("id", `${lineIdx}`);
+    txtDiv.appendChild(p);
+    typingLine();
+    if (lineIdx < txt.length-1) {
+        setTimeout(() => {
+            lineIdx++;
+            letterIdx = 0;
+
+            typingEffect();
+        },3000);
+    }
+  }
+
+  function typingLine() {
+    if (letterIdx < txt[lineIdx].length) {
+        const txtId = document.getElementById(`${lineIdx}`).innerHTML += txt[lineIdx].charAt(letterIdx);
+        letterIdx++;
+        setTimeout(typingLine, 30);
+    }
+  }
+
+  function animateCharacter() {
+    if (animationCount >= instructionsAnimation.length) {
+        animationCount = 0;
+    }
+    instructionsCtx.clearRect(0,0,150,200);
+    instructionsCtx.drawImage(instructionsAnimation[animationCount],0,0,150,200);
+    animationCount++;
+    setTimeout(animateCharacter,500);
+  }
   
+  // Main Loop
+
   startButton.onclick = function () {
-    let audio = createSound(instructionsMusic);
-    playSong(audio);
+    playSong(instructionsAudio);
     startPage.style = "display: none";
     instructionsPage.classList.remove('hidden');
+    animateCharacter();
+    typingEffect();
     setTimeout(() => {
-      stopSong(audio);
+      stopSong(instructionsAudio);
       audio = createSound(gameMusic);
       playSong(audio);
       instructionsPage.classList.add('hidden');
@@ -37,4 +86,3 @@ window.onload = function () {
     
   }
 }
-
