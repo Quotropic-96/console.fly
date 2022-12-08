7dc69c4e9beddc394ab66dd1d336786852632d54
Gerard Solanes Hernandez <gerardsolher@gmail.com>
2022-12-08 17:54:25 +0100

    Change rutes for assets

--- js/assets.js
+++ js/assets.js
@@ -1,81 +1,81 @@
 // Background
 const bgImg0 = new Image();
-bgImg0.src = '../assets/bg/bg_0.png';
+bgImg0.src = './assets/bg/bg_0.png';
 
 const bgImg1 = new Image();
-bgImg1.src = '../assets/bg/bg_1.png';
+bgImg1.src = './assets/bg/bg_1.png';
 
 const bgImg2 = new Image();
-bgImg2.src = '../assets/bg/bg_2.png';
+bgImg2.src = './assets/bg/bg_2.png';
 
 const bgImg3 = new Image();
-bgImg3.src = '../assets/bg/bg_3.png';
+bgImg3.src = './assets/bg/bg_3.png';
 
 const bg = [bgImg0, bgImg1, bgImg2, bgImg3];
 
 // Floor 
 const floor = new Image();
-floor.src = '../assets/bg/floor.png';
+floor.src = './assets/bg/floor.png';
 
 // Player
 
 //      Run
 const playerRun0 = new Image();
-playerRun0.src = '../assets/player/run_0.png';
+playerRun0.src = './assets/player/run_0.png';
 
 const playerRun1 = new Image();
-playerRun1.src = '../assets/player/run_1.png';
+playerRun1.src = './assets/player/run_1.png';
 
 const playerRun2 = new Image();
-playerRun2.src = '../assets/player/run_2.png';
+playerRun2.src = './assets/player/run_2.png';
 
 const playerRun3 = new Image();
-playerRun3.src = '../assets/player/run_3.png';
+playerRun3.src = './assets/player/run_3.png';
 
 const playerRun4 = new Image();
-playerRun4.src = '../assets/player/run_4.png';
+playerRun4.src = './assets/player/run_4.png';
 
 const playerRun5 = new Image();
-playerRun5.src = '../assets/player/run_5.png';
+playerRun5.src = './assets/player/run_5.png';
 
 const playerRun = [playerRun0, playerRun1, playerRun2, playerRun3, playerRun4, playerRun5];
 
 //      Jump
 const playerJump0 = new Image();
-playerJump0.src = '../assets/player/jump_0.png';
+playerJump0.src = './assets/player/jump_0.png';
 
 const playerJump1 = new Image();
-playerJump1.src = '../assets/player/jump_1.png';
+playerJump1.src = './assets/player/jump_1.png';
 
 const playerJump2 = new Image();
-playerJump2.src = '../assets/player/jump_2.png';
+playerJump2.src = './assets/player/jump_2.png';
 
 const playerJump3 = new Image();
-playerJump3.src = '../assets/player/jump_3.png';
+playerJump3.src = './assets/player/jump_3.png';
 
 const playerJump = [playerJump0, playerJump1, playerJump2, playerJump3];
 
 //       Fly
 const playerFly0 = new Image();
-playerFly0.src = '../assets/player/fly_0.png';
+playerFly0.src = './assets/player/fly_0.png';
 
 const playerFly1 = new Image();
-playerFly1.src = '../assets/player/fly_1.png';
+playerFly1.src = './assets/player/fly_1.png';
 
 const playerFly2 = new Image();
-playerFly2.src = '../assets/player/fly_2.png';
+playerFly2.src = './assets/player/fly_2.png';
 
 const playerFly3 = new Image();
-playerFly3.src = '../assets/player/fly_3.png';
+playerFly3.src = './assets/player/fly_3.png';
 
 const playerFly = [playerFly0, playerFly1, playerFly2, playerFly3];
 
 //      Fall
 const playerFall0 = new Image();
-playerFall0.src = '../assets/player/fall_0.png';
+playerFall0.src = './assets/player/fall_0.png';
 
 const playerFall1 = new Image();
-playerFall1.src = '../assets/player/fall_1.png';
+playerFall1.src = './assets/player/fall_1.png';
 
 const playerFall = [playerFall0, playerFall1];
 
@@ -91,127 +91,127 @@ const playerIdle = [playerIdle0, playerIdle3];
 
 // Missile
 const missile0 = new Image();
-missile0.src = '../assets/missile/missile_0.png';
+missile0.src = './assets/missile/missile_0.png';
 
 const missile1 = new Image();
-missile1.src = '../assets/missile/missile_1.png';
+missile1.src = './assets/missile/missile_1.png';
 
 const missile2 = new Image();
-missile2.src = '../assets/missile/missile_2.png';
+missile2.src = './assets/missile/missile_2.png';
 
 const missile3 = new Image();
-missile3.src = '../assets/missile/missile_3.png';
+missile3.src = './assets/missile/missile_3.png';
 
 const missile4 = new Image();
-missile4.src = '../assets/missile/missile_4.png';
+missile4.src = './assets/missile/missile_4.png';
 
 missileMoveLeft = [missile0, missile1, missile2, missile3, missile4];
 
 // Zappers
 // Horizontal
 const zaph0 = new Image();
-zaph0.src = '../assets/zapper/horizontal/zap_0.png';
+zaph0.src = './assets/zapper/horizontal/zap_0.png';
 
 const zaph1 = new Image();
-zaph1.src = '../assets/zapper/horizontal/zap_1.png';
+zaph1.src = './assets/zapper/horizontal/zap_1.png';
 
 const zaph2 = new Image();
-zaph2.src = '../assets/zapper/horizontal/zap_2.png';
+zaph2.src = './assets/zapper/horizontal/zap_2.png';
 
 const zaph3 = new Image();
-zaph3.src = '../assets/zapper/horizontal/zap_3.png';
+zaph3.src = './assets/zapper/horizontal/zap_3.png';
 
 const zaph4 = new Image();
-zaph4.src = '../assets/zapper/horizontal/zap_4.png';
+zaph4.src = './assets/zapper/horizontal/zap_4.png';
 
 const zaph5 = new Image();
-zaph5.src = '../assets/zapper/horizontal/zap_5.png';
+zaph5.src = './assets/zapper/horizontal/zap_5.png';
 
 const zaph6 = new Image();
-zaph6.src = '../assets/zapper/horizontal/zap_6.png';
+zaph6.src = './assets/zapper/horizontal/zap_6.png';
 
 const zaph7 = new Image();
-zaph7.src = '../assets/zapper/horizontal/zap_7.png';
+zaph7.src = './assets/zapper/horizontal/zap_7.png';
 
 const zaph8 = new Image();
-zaph8.src = '../assets/zapper/horizontal/zap_8.png';
+zaph8.src = './assets/zapper/horizontal/zap_8.png';
 
 const zaph9 = new Image();
-zaph9.src = '../assets/zapper/horizontal/zap_9.png';
+zaph9.src = './assets/zapper/horizontal/zap_9.png';
 
 const zaph10 = new Image();
-zaph10.src = '../assets/zapper/horizontal/zap_10.png';
+zaph10.src = './assets/zapper/horizontal/zap_10.png';
 
 const zapperHAnimation = [zaph0, zaph1, zaph2, zaph3, zaph4, zaph5, zaph6, zaph7, zaph8, zaph9, zaph10];
 
 // Vertical
 
 const zapv0 = new Image();
-zapv0.src = '../assets/zapper/vertical/zap_0.png';
+zapv0.src = './assets/zapper/vertical/zap_0.png';
 
 const zapv1 = new Image();
-zapv1.src = '../assets/zapper/vertical/zap_1.png';
+zapv1.src = './assets/zapper/vertical/zap_1.png';
 
 const zapv2 = new Image();
-zapv2.src = '../assets/zapper/vertical/zap_2.png';
+zapv2.src = './assets/zapper/vertical/zap_2.png';
 
 const zapv3 = new Image();
-zapv3.src = '../assets/zapper/vertical/zap_3.png';
+zapv3.src = './assets/zapper/vertical/zap_3.png';
 
 const zapv4 = new Image();
-zapv4.src = '../assets/zapper/vertical/zap_4.png';
+zapv4.src = './assets/zapper/vertical/zap_4.png';
 
 const zapv5 = new Image();
-zapv5.src = '../assets/zapper/vertical/zap_5.png';
+zapv5.src = './assets/zapper/vertical/zap_5.png';
 
 const zapv6 = new Image();
-zapv6.src = '../assets/zapper/vertical/zap_6.png';
+zapv6.src = './assets/zapper/vertical/zap_6.png';
 
 const zapv7 = new Image();
-zapv7.src = '../assets/zapper/vertical/zap_7.png';
+zapv7.src = './assets/zapper/vertical/zap_7.png';
 
 const zapv8 = new Image();
-zapv8.src = '../assets/zapper/vertical/zap_8.png';
+zapv8.src = './assets/zapper/vertical/zap_8.png';
 
 const zapv9 = new Image();
-zapv9.src = '../assets/zapper/vertical/zap_9.png';
+zapv9.src = './assets/zapper/vertical/zap_9.png';
 
 const zapv10 = new Image();
-zapv10.src = '../assets/zapper/vertical/zap_10.png';
+zapv10.src = './assets/zapper/vertical/zap_10.png';
 
 const zapperVAnimation = [zapv0, zapv1, zapv2, zapv3, zapv4, zapv5, zapv6, zapv7, zapv8, zapv9, zapv10];
 
 // Diagonal
 const eb0 = new Image();
-eb0.src = '../assets/zapper/diagonal/eb_0.png';
+eb0.src = './assets/zapper/diagonal/eb_0.png';
 
 const eb1 = new Image();
-eb1.src = '../assets/zapper/diagonal/eb_1.png';
+eb1.src = './assets/zapper/diagonal/eb_1.png';
 
 const eb2 = new Image();
-eb2.src = '../assets/zapper/diagonal/eb_2.png';
+eb2.src = './assets/zapper/diagonal/eb_2.png';
 
 const eb3 = new Image();
-eb3.src = '../assets/zapper/diagonal/eb_3.png';
+eb3.src = './assets/zapper/diagonal/eb_3.png';
 
 const eb4 = new Image();
-eb4.src = '../assets/zapper/diagonal/eb_4.png';
+eb4.src = './assets/zapper/diagonal/eb_4.png';
 
 const eb5 = new Image();
-eb5.src = '../assets/zapper/diagonal/eb_5.png';
+eb5.src = './assets/zapper/diagonal/eb_5.png';
 
 const eb6 = new Image();
-eb6.src = '../assets/zapper/diagonal/eb_6.png';
+eb6.src = './assets/zapper/diagonal/eb_6.png';
 
 const eBAnimation = [eb0, eb1, eb2, eb3, eb4, eb5, eb6];
 
 // Music
-const instructionsMusic = '../assets/sound/songs/Instructions0.m4a';
+const instructionsMusic = './assets/sound/songs/Instructions0.m4a';
 
-const gameMusic = '../assets/sound/songs/Game0.m4a';
+const gameMusic = './assets/sound/songs/Game0.m4a';
 
-const endMusic = '../assets/sound/songs/EndGame0.m4a';
+const endMusic = './assets/sound/songs/EndGame0.m4a';
 
 // Sound Effects
-const typingAudio = '../assets/sound/sound_effects/typing.wav';
+const typingAudio = './assets/sound/sound_effects/typing.wav';
 
